e3097ac00e97ed20deecae20b7fce0e297d8c69b
Walter Soprani Jr <91127730+WalterSopraniJr@users.noreply.github.com>
2022-04-13 19:39:41 -0300

    Método para transferir dinheiro e teste de mesa.

--- byteBank/src/TestaMetodo.java
+++ byteBank/src/TestaMetodo.java
@@ -7,6 +7,20 @@ public class TestaMetodo {
 		contaDoUsuario.deposita(50);
 		System.out.println(contaDoUsuario.saldo);
 		
+		boolean conseguiuRetirar = contaDoUsuario.saca(20);
+		System.out.println(contaDoUsuario.saldo);
+		System.out.println(conseguiuRetirar);
+		
+		Conta contaBeatriz = new Conta();
+		contaBeatriz.deposita(1000);
+		boolean sucessoTransferencia = contaBeatriz.transfere(2000, contaDoUsuario);
+		if (sucessoTransferencia) {
+			System.out.println("Transferência com sucesso");
+		} else {
+			System.out.println("Faltou dinheiro.");
+		}
+		System.out.println(contaBeatriz.saldo);
+		System.out.println(contaDoUsuario.saldo);
 	}
 
 }
