4438f238c5003f072976df111926bd85c89ce8f6
Walter Soprani Jr <91127730+WalterSopraniJr@users.noreply.github.com>
2022-04-21 22:13:54 -0300

    update e testes

--- bytebank-encapsulado/src/TestaGetESet.java
+++ bytebank-encapsulado/src/TestaGetESet.java
@@ -1,4 +1,28 @@
 
 public class TestaGetESet {
 
+	public static void main(String[] args) {
+		Conta conta = new Conta();
+		conta.setNumero(1337);
+		System.out.println(conta.getNumero());
+		
+		Cliente walter = new Cliente();
+		conta.setTitular(walter);
+		walter.setNome("Walter Soprani");
+		
+		System.out.println(conta.getTitular().getNome());
+		
+		conta.getTitular().setProfissao("Programador");
+		System.out.println(conta.getTitular().getProfissao());
+		
+		Cliente titularDaConta = conta.getTitular();
+		titularDaConta.setProfissao("Programador");
+		
+		
+		//observando que tudo chega ao mesmo endereco de memoria
+		System.out.println(titularDaConta);
+		System.out.println(walter);
+		System.out.println(conta.getTitular());
+		
+		}
 }
