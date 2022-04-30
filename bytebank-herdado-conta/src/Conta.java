278cae4f3ec6109da3633d122173ab7e3e047299
Walter Soprani Jr <91127730+WalterSopraniJr@users.noreply.github.com>
2022-04-29 22:54:06 -0300

    Criando conta corrente e poupanca
    Repetindo as declarações do construtor da classe Conta (agencia e numero) nós conseguimos reutilizar o construtor da super classe.

---
+++ bytebank-herdado-conta/src/Conta.java
@@ -0,0 +1,87 @@
+public class Conta {
+	private double saldo; //não permite ser modificado fora da classe
+	private int agencia;
+	private int numero;
+	private Cliente titular;
+	private static int total = 0;  //statico da classe, para não ser construido varios independentes por conta.
+	
+	public Conta(int agencia, int numero) {
+		total++;
+		System.out.println("O total de contas é " + Conta.total);
+		this.agencia = agencia;
+		this.numero = numero;
+		this.saldo = 0;
+		System.out.println("Criando uma conta " + this.numero);
+	}
+	
+	void deposita(double valor) {
+		this.saldo += valor;
+	}
+	
+	public boolean saca(double valor) {
+		if(this.saldo >= valor) {
+			this.saldo -= valor;
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	public boolean transfere(double valor, Conta destino) {
+		if(this.saldo >= valor) {
+			this.saldo -= valor;
+			destino.deposita(valor);
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	public double getSaldo() {  //método getter para o saldo
+		return this.saldo;
+	}
+	
+	public int getNumero() {
+		return this.numero;
+	}
+	
+	public void setNumero(int numero) {
+		if(numero <= 0) {
+			System.out.println("Coloque um valor maior que zero no numero");
+			return;
+		}
+		this.numero = numero;
+	}
+	
+	public int getAgencia() {
+		return this.agencia;
+	}
+	
+	public void setAgencia(int agencia) {
+		if(agencia <= 0) {
+			System.out.println("Coloque um valor maior que zero na agencia");
+			return;
+		}
+		this.agencia = agencia;
+	}
+	
+	public void setTitular(Cliente titular) {
+		this.titular = titular;
+	}
+	
+	public Cliente getTitular() {
+		return this.titular;
+	}
+	
+	public static int getTotal() {
+		return Conta.total;
+	}
+
+	public void setNome(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+	
+}
