e2f517431e505858bcf6cf48d0ea22efc209b6d7
Walter Soprani Jr <91127730+WalterSopraniJr@users.noreply.github.com>
2022-04-30 21:28:35 -0300

    Aplicação de abstract em funcionario.
    Funcionario é a base das demais classes, porém, não pode ser declarado, pois ele não é um cargo específico.

--- bytebank-herdado/src/TesteFuncionario.java
+++ bytebank-herdado/src/TesteFuncionario.java
@@ -3,7 +3,7 @@ public class TesteFuncionario {
 	
 	public static void main(String[] args) {
 		
-		Funcionario nico = new Funcionario();
+		Funcionario nico = new Gerente();
 		nico.setNome("Nico di Angelo");
 		nico.setCpf("222.222.222-22");
 		nico.setSalario(2660.00);
--- bytebank-herdado/src/TesteReferencias.java
+++ bytebank-herdado/src/TesteReferencias.java
@@ -9,14 +9,10 @@ public class TesteReferencias {
 		
 		Supervisor marcelo = new Supervisor();
 		marcelo.setSalario(3000.00);		
-		
-		Funcionario marcos = new Funcionario();
-		marcos.setSalario(2000.00);	
-		
+				
 		ControleBonificacao controle = new ControleBonificacao();
 		controle.registra(james);
 		controle.registra(marcelo);
-		controle.registra(marcos);
 		
 		System.out.println("Total pago pela empresa em bonificação: R$ " + controle.getSoma());
 	}
