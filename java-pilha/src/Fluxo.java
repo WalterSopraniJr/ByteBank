fe05524422299ce7abbcde921483adf83af6a1fc
Walter Soprani Jr <91127730+WalterSopraniJr@users.noreply.github.com>
2022-05-05 23:07:40 -0300

    Foi 'plantado' um erro dividindo por zero.
    Utilizado try catch para identificar o erro, imprimir no console e continuar o programa (Exceções).

--- java-pilha/src/Fluxo.java
+++ java-pilha/src/Fluxo.java
@@ -2,6 +2,7 @@
 public class Fluxo {
 
     public static void main(String[] args) {
+    	
         System.out.println("Ini do main");
         metodo1();
         System.out.println("Fim do main");
@@ -17,6 +18,12 @@ public class Fluxo {
         System.out.println("Ini do metodo2");
         for(int i = 1; i <= 5; i++) {
             System.out.println(i);
+            try {
+            	int a = i / 0;
+            } catch(ArithmeticException ex) {
+            	System.out.println("ArithmeticException");
+            }
+            
         }
         System.out.println("Fim do metodo2");
     }
